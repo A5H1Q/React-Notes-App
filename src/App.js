893c82b42eb9f5d767bb992962c058209c0320f9
A5H1Q <46376026+A5H1Q@users.noreply.github.com>
2021-09-21 15:14:24 +0530

    Updated UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useState, useEffect} from "react";
+import {ReactComponent as AddIcon} from "./plus.svg";
+import {ReactComponent as DarkIcon} from "./dark.svg";
+import {ReactComponent as LightIcon} from "./bright.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+ const [darkMode, applyDark] = useState(false);
+ const [msg, toggleMsg] = useState(false);
+ const [notes, notesList] = useState([{id: 123, text: ""}]);
+
+ const Navbar = (props) => <header>{props.children}</header>;
+
+ const toggleDMode = (x) => {
+  applyDark(!x);
+  var root = document.querySelector(":root");
+  if (x) {
+   root.style.setProperty("--nav", "#fff");
+   root.style.setProperty("--txt", "#000");
+   root.style.setProperty("--btn", "#d2d2d2");
+   root.style.setProperty("--shadow", "#a9a9a9");
+  } else {
+   root.style.setProperty("--nav", "#202020");
+   root.style.setProperty("--txt", "#fff");
+   root.style.setProperty("--btn", "#4e4e4e");
+   root.style.setProperty("--shadow", "#363636");
+  }
+ };
+
+ useEffect(() => {
+  notes.length === 0 ? toggleMsg(true) : toggleMsg(false);
+ }, [notes]);
+
+ return (
+  <div className="App">
+   <Navbar>
+    <h3>Notes App</h3>
+    <div>
+     <AddIcon
+      onClick={() => {
+       notesList([...notes, {id: Date.now(), text: ""}]);
+      }}
+     />
+     {darkMode ? (
+      <LightIcon
+       onClick={() => {
+        toggleDMode(true);
+       }}
+      />
+     ) : (
+      <DarkIcon
+       onClick={() => {
+        toggleDMode(false);
+       }}
+      />
+     )}
     </div>
-  );
-}
+   </Navbar>
 
-export default App;
+   <div className="list">
+    {notes.map((item) => (
+     <div className="note" key={item.id}>
+      <div className="titlebar">
+       <div
+        className="close"
+        onClick={(e) => {
+         notesList(notes.filter((notes) => notes.id !== item.id));
+        }}
+       >
+        -
+       </div>
+      </div>
+      {/* Height-Responsive textarea  */}
+      <textarea
+       placeholder="Type here.."
+       defaultValue={item.text}
+       onInput={(e) => {
+        e.target.style.height = "205px";
+        e.target.style.height = e.target.scrollHeight + "px";
+       }}
+      ></textarea>
+     </div>
+    ))}
+   </div>
+   {msg ? <span id="msg">Click '+' to Add a new Note</span> : null}
+  </div>
+ );
+}
